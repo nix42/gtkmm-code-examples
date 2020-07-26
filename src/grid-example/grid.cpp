b0f69fa212b60598c5dfd3a0cfc69f3b2dee685d
nix42 <louis2000465@protonmail.com>
2020-07-26 10:34:27 +0100

    grid example complete

--- src/grid-example/grid.cpp
+++ src/grid-example/grid.cpp
@@ -2,13 +2,29 @@
 
 Grid::Grid() {
     set_title("Grid example");
-    set_size_request(400, 300);
-    add(mGrid);
+    set_size_request(200, 200);
+    add(mBox);
 
-    mGrid.set_homogeneous(true);
+    mBox.set_homogeneous(true);
+    mBox.set_center_widget(mGrid);
 
+    mLabel1.set_label("This");
+    mLabel2.set_label("is");
+    mLabel3.set_label("a grid");
+
+    mButton.set_label("exit");
+    mButton.signal_clicked().connect(sigc::mem_fun(*this, &Grid::on_button_exit));
+
+    mGrid.attach(mLabel1, 1, 1);
+    mGrid.attach(mLabel2, 2, 1);
+    mGrid.attach(mLabel3, 3, 1);
+    mGrid.attach_next_to(mButton, mLabel2, Gtk::POS_BOTTOM);
 
     show_all_children();
+}
+
+void Grid::on_button_exit() {
+    Gtk::Window::hide();
 }
 
 Grid::~Grid() {}
--- src/grid-example/grid.h
+++ src/grid-example/grid.h
@@ -3,7 +3,12 @@
  
 class Grid : public Gtk::Window {
 protected:
+    Gtk::Box mBox;
     Gtk::Grid mGrid;
+    Gtk::Label mLabel1, mLabel2, mLabel3;
+    Gtk::Button mButton;
+
+    void on_button_exit();
     
 public:
     Grid();
